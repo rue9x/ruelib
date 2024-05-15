48fcbad8becf69080dfdeaa5bdb2541c2ad126c0
Rue Lazzaro <rue.lazzaro@gmail.com>
2024-05-15 03:13:04 -0400

    Added a few more functions.

--- ruelib/ruelib.py
+++ ruelib/ruelib.py
@@ -7,6 +7,10 @@ import json
 import threading
 from queue import Queue
 import base64
+import winsound
+import random
+from turtle import *
+
 
 class Worker(threading.Thread):
     '''
@@ -44,6 +48,60 @@ class ThreadPool(object):
     def wait_complete(self):
         self.q.join()
 
+def playsound(fn,flags):
+    '''
+    Plays a wav file. Library allows flags to be passed, but I'm not sure what they are.
+    If fn is "None" it will stop playing any sound that's currently playing.
+    '''
+    if flags:
+        winsound.PlaySound(sound=fn,flags=flags)
+    else:
+        winsound.PlaySound(sound=fn)
+
+def beep(type=None):
+    '''
+    Plays a windows beep. Type can be the following (default is asterisk):
+    'SystemAsterisk'
+    'SystemExclamation' 
+    'SystemExit'
+    'SystemHand'
+    'SystemQuestion'
+    '''
+    if type:
+        winsound.MessageBeep(type=type)
+    else:
+        winsound.MessageBeep()    
+
+def roll(dice='1d6',plus=0,minus=0,seed=None):
+    '''
+    Dice roller. dice is a string containing the D&D verbiage for the dice ("numdice d sizeofdice"). 
+    You can optionally add a value to the total, subtract, or force the rng to a specific seed.
+    '''
+    num_dice = int(dice.lower().split("d")[0])
+    size_dice = int(dice.lower().split("d")[1])
+    total = 0
+    for i in range (num_dice):
+        if seed:
+            random.seed(seed)
+        total=total+random.randint(1,size_dice)
+    total = total+plus
+    total = total-minus
+    if total < 0:
+        total = 0
+    return total
+
+
+def maybe(fun):
+    ''' 
+    A simple one line try/except. 
+    fun is the function you want to call (example: maybe(roll("1d6")) or maybe(print("test")))
+    '''
+    try:
+        return fun
+    except Exception as Ex:
+        return Ex
+
+    
 def tob64(s):
     ''' 
     Converts text string into base64
